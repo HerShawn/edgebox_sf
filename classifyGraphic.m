393d92aeb80b5c35051cfba7487a89e3c85e5299
her shawn <hexiangman@qq.com>
2016-10-16 23:00:01 +0800

    2016-10-16
    【一个像素宽的边缘应当删去】

--- classifyGraphic.m
+++ classifyGraphic.m
@@ -1,5 +1,5 @@
 
-% 图形学判别
+% 形态学判别
 function [afterBBoxes,bbox]=classifyGraphic(skeletImg)
 
 % 连通区域的属性
@@ -12,21 +12,41 @@ bbox = vertcat(mserStats.BoundingBox);
 w = bbox(:,3);
 h = bbox(:,4);
 aspectRatio = w./h;
-bboxArea=w.*h;
 %宽高比
-filterIdx = aspectRatio' > 10 ;
-filterIdx = filterIdx |  aspectRatio' <0.2 ;
+filterIdx = aspectRatio' > 7 ;
+filterIdx = filterIdx |  aspectRatio' <1/7 ;
 %滤掉太小面积的
-filterIdx = filterIdx | [mserStats.FilledArea] < 50 ;
-filterIdx = filterIdx | [mserStats.ConvexArea]./bboxArea' < 0.5 ;
+filterIdx = filterIdx | [mserStats.FilledArea] <30  ;
+
+
 %欧拉数和（面积排在前两位）结合起来用
-[~,index] = sortrows([mserStats.Area].');
-thresh_Area=mserStats(index(end-2)).Area;
-filterIdx = filterIdx | ([mserStats.EulerNumber] <= -10 )&([mserStats.Area]>=thresh_Area);
+% [~,index] = sortrows([mserStats.Area].');
+% thresh_Area=mserStats(index(end-2)).Area;
+% filterIdx = filterIdx | ([mserStats.EulerNumber] <= -10 )&([mserStats.Area]>=thresh_Area);
 %图形学和稳定性结合起来；存在上述情况，说明存在较严重的粘连情况，则直接跳到下一轮稳定性；
+
+%
+
 mserStats(filterIdx) = [];
 clear filterIdx
 bbox = vertcat(mserStats.BoundingBox);
+
+%滤除掉内含bbox数目较多的边缘CC中
+adjoin= bboxOverlapRatio(bbox, bbox,'Min');
+% 设bbox与它自己没有连通关系
+n = size(adjoin,1);
+adjoin(1:n+1:n^2) = 0;
+%计算每个bbox完全内含其它bbox的个数
+adj_index=zeros(1,n);
+for adj=1:n
+    adj_index(1,adj)=length(find(adjoin(adj,:)==1));
+end
+%大于内含bbox数目中值的两倍的bbox在检测粘连点的考虑范围内
+adj_thresh=max(1,median(adj_index))*7;
+adjIdx=adj_index>adj_thresh;
+mserStats(adjIdx)= [];
+bbox = vertcat(mserStats.BoundingBox);
+
 
 %图形学判别后的结果展示
 afterBBoxes =insertShape(skeletImg,'Rectangle',bbox,'LineWidth',1);
