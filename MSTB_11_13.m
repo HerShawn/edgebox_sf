32033e8a341845a95a40aa44f8ea96b2811a3068
her shawn <hexiangman@qq.com>
2016-11-13 22:21:20 +0800

    2016-11-13-2
    2.组内部的融合：
    中值、方向线

--- MSTB_11_13.m
+++ MSTB_11_13.m
@@ -26,7 +26,7 @@ num_img = length(dir_img);
 load('initialSfIdx');
 eIdx=[];
 e10Idx=[];
-for indexImg = 36:39
+for indexImg = 1:num_img
     fusionBBox=[];
     img_value = dir_img(indexImg).name;
     img_value = img_value(1:end-4);
@@ -68,13 +68,42 @@ for indexImg = 36:39
     xmax = min(xmax, size(g,2));
     ymax = min(ymax, size(g,1));
     expandedBBoxes = [xmin ymin xmax-xmin+1 ymax-ymin+1];
-    % bbox作为结点，linjie
+    % bbox作为结点，邻接关系作为边，建图
+    %（1）将bbox构成文本行组的两个限制：height之比、偏移量
     overlapRatio = bboxOverlap(expandedBBoxes, expandedBBoxes);
     n = size(overlapRatio,1);
     overlapRatio(1:n+1:n^2) = 0;
     gh = graph(overlapRatio);
-    % Find the connected text regions within the graph
+    % 在图中找出连通的文本区域
     componentIndices = conncomp(gh);
+    % 构造refine用表：bbox序号，bbox中心点的横、纵坐标，bbox的高度
+    refine_matrix=zeros(size(fusionBBox,1),3);
+    refine_matrix(:,1)=fusionBBox(:,1)+fusionBBox(:,3)/2;
+    refine_matrix(:,2)=fusionBBox(:,2)+fusionBBox(:,4)/2;
+    refine_matrix(:,3)=fusionBBox(:,4);
+    % （2）组内部的融合：height中值、方向线
+    axis([0 size(g,2) 0 size(g,1)]);
+    set(gca, 'YDir','reverse');
+    hold on
+    for i=1:max(componentIndices)
+        txtGroup=find(componentIndices==i);
+        for ii=1:size(txtGroup,2)
+            j=txtGroup(1,ii);
+            HY=refine_matrix(j,2)-refine_matrix(j,3)/2:refine_matrix(j,2)+refine_matrix(j,3)/2;
+            HX=refine_matrix(j,1)*ones(1,length( HY));
+            plot(HX,HY);
+        end
+        LY=refine_matrix(txtGroup,2);
+        LX=refine_matrix(txtGroup,1);
+        plot(LX,LY,'-ro',...
+            'LineWidth',0.5,...
+            'MarkerSize',2,...
+            'MarkerEdgeColor','b');
+        HY=[]; HX=[]; LY=[]; LX=[];
+    end
+    hold off
+    saveas(gcf,[img_value  '-intra.bmp']);
+    close all
 end
 
 
